3f36e571d3a2c169403df565d36c4c755787798a
linshanshan <1570842245@qq.com>
2017-05-05 09:14:57 +0800

    flex.html b1

---
+++ Zzj/example.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+        <title>驾驶证相片回执</title> 
+        <style type="text/css">
+            img{width: 100%;}
+        </style>
+    </head>
+    <body>
+        <img src="<?php echo base_url('assets/images/selfServiceMachineGuide/example'.$type.'.jpg')?>">
+    </body>
+</html>
